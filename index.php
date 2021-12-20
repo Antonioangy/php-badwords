5d18b27e4dcbfca736157f7ae8e6118c1195d5ac
Antonioangy <dumitruangy26@gmail.com>
2021-12-20 15:22:07 +0100

    prima volta su php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo 'ciao Antonio'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        *{
+            background-color: yellow;
+            text-align: center;
+        }
+    </style>
+
+    <?php
+        $x = "la mia prima volta";
+        $y = "su php";
+        $z = $x . $y;
+    ?>
+    <?php
+        echo $z;
+    ?>
+</body>
+</html>
